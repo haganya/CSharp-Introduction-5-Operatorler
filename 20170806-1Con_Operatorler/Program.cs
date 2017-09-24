51d745bb256652574ec29bb00bdf2261d385d4fe
Hakan Akgül <hakanakgul34@gmail.com>
2017-09-24 20:05:38 +0300

    created at 06.08.2017 (dd.mm.yyyy)

---
+++ 20170806-1Con_Operatorler/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20170806_1Con_Operatorler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // operatorler c# tarafında anlamlı işaretlerdir.
+            /*
+             * Matematiksel Operatorler
+             * --------------------------------------------------------------------
+             * + - * / (toplama, çıkarma, çarpma, bölme)
+             * ++ değeri 1 arttır.
+             * -- değeri 1 azalt.
+             * % mod alma operatorüdür. değerin 10 a bölümünden kalanı verir.
+             *  += , -= , *= , /=  -> bu operatorler tekrarı azaltmak içindir.
+             *  örn. a+=5 ile a = a+5 aynı ifadeyi temsil eder.
+             *  = operatoru atama işlemi yapar. sağındaki operandın değerini, solundaki operandın değeri olarak atar.
+             *  
+             *  
+             *  İlişkisel Operatorler
+             *  -------------------------------------------------------------------
+             *  ilişkisel operatorler geriye bool döndürür. (true veya false)
+             *  == eşit mi
+             *  < küçük mü
+             *  > büyük mü
+             *  <= küçük veya eşit mi
+             *  >= büyük veya eşit mi
+             *  != eşit değil mi
+             *  
+             *  
+             *  Mantıksal Operatorler
+             *  -------------------------------------------------------------------
+             *  & ve işareti : operandlardan ikiside sağlanıyorsa true, diğer durumlarda false
+             *  | veya işareti : operandlardan ikiside sağlanmıyorsa false, diğer durumlarda true
+             *  ! değil işareti : true ise false , false ise true
+             *  
+             *  
+             */
+        }
+    }
+}
